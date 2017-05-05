bb984019399b0b0be135e64d7c8260c07c081e82
Alfredo0688 <alfre0688@hotmail.com>
2017-05-05 15:47:39 -0300

    Pruebas

---
+++ Variables-Operaciones/1_4_Aritmetica.vb
@@ -0,0 +1,22 @@
+﻿Module _1_4Aritmetica
+    Sub main()
+
+        Dim valor As Short = 1250
+        Dim valor2 As Integer = 851
+
+        Dim valor3 As Single = 1.8465
+        Dim valor4 As Double = 1.9846432131
+
+        Console.WriteLine("Valor short: "& valor ) 
+        Console.WriteLine("Valor entero: " & valor2)
+        Console.WriteLine("Valor single: " & valor3)
+        Console.WriteLine("Valor double: " & valor4)
+
+        Console.WriteLine("Sumar entero short con punto flotante double: " & valor + valor4)
+        Console.WriteLine("Restar single con integer: " & valor3 - valor2)
+        Console.WriteLine("Sumar entero con entero: " & valor + valor2)
+        Console.WriteLine("sumar single y double: " & valor3 + valor4)
+        Console.WriteLine("restar double con single: " & valor3 - valor4)
+        Console.ReadKey()
+    End Sub
+End Module
---
+++ Variables-Operaciones/1_Variables.vb
@@ -0,0 +1,28 @@
+﻿Module Variables
+
+    Public nombre_persona As String = "Ricardo" 'a nivel solucion
+    Friend tipo_documento As String = "DNI" 'a nivel proyecto
+    Friend numero_documento As String = "30222555"
+    Friend fecha_nacimiento As Date = #11/25/1980#
+    Private localidad As String = "Rosario" ' a nivel modulo o clase
+    Private altura As Single = 1.85
+    Private estado_civil As String = "Viudo"
+    Private argentino As String = "si"
+
+    Sub Main()
+        Dim edad As Byte = 37 'byte sin signo de 0 a 255
+
+        Console.WriteLine("Nombre: " & nombre_persona)
+        Console.WriteLine("Tipo de documento: " & tipo_documento)
+        Console.WriteLine("Numero de documento: " & numero_documento)
+        Console.WriteLine("Localidad: " & localidad)
+        Console.WriteLine("Fecha de nacimiento" & fecha_nacimiento)
+        Console.WriteLine("Edad: " & edad)
+        Console.WriteLine("Altura corporal: " & altura)
+        Console.WriteLine("Estado civil: " & estado_civil)
+        Console.WriteLine("Argentino?: " & argentino)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
